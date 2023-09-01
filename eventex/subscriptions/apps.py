9708249137a514e93ab578def777870aeba6207b
Vitorpestana94 <vitorpestanatr@gmail.com>
2023-09-01 18:22:46 -0300

    Admin-magic

--- eventex/subscriptions/apps.py
+++ eventex/subscriptions/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SubscriptionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eventex.subscriptions'
+    verbose_name = 'Controle de Participantes'
