d13ee68aa89e56b8c2e6f08d5bf84a1459ba2e41
Vitorpestana94 <vitorpestanatr@gmail.com>
2023-09-01 16:14:20 -0300

    Instala django-extensions

--- eventex/subscriptions/tests/test_mail_subscribe.py
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 class SubscribePostValid(TestCase):
     def setUp(self):
-        data = dict(name='Henrique Bastos',cpf='12345678901', email='henrique@bastos.net', phone='21-99618-6180')
-        self.client.post('/inscricao/', data)
+        self.data = {'name':'Henrique Bastos','cpf':'12345678901', 'email':'henrique@bastos.net', 'phone':'21-99618-6180'}
+        self.client.post('/inscricao/', self.data)
         self.email = mail.outbox[0]
         self.default_gmail = settings.DEFAULT_FROM_EMAIL
     
@@ -19,10 +19,10 @@ class SubscribePostValid(TestCase):
         self.assertEqual([self.default_gmail, 'henrique@bastos.net'], self.email.to)
     
     def test_subscription_email_body(self):
-        contents = ['Henrique Bastos',
-                '12345678901',
-                'henrique@bastos.net',
-                '21-99618-6180']
+        contents = [self.data['name'],
+                self.data['cpf'],
+                self.data['email'],
+                self.data['phone']]
         for content in contents:
             with self.subTest():
                 self.assertIn(content, self.email.body)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -10,12 +10,22 @@ from django.conf import settings
 def subscribe(request):
     return create(request) if request.method == 'POST' else new(request)
 
+
 def create(request):
     form = SubscriptionForm(request.POST)
+    return render(request, 'subscriptions/subscription_form.html', {'form':form})  if not form.is_valid() else _success(request, form)
+    
 
-    if not form.is_valid():
-        return render(request, 'subscriptions/subscription_form.html', {'form':form})
+def new(request):
+    return render(request,  'subscriptions/subscription_form.html', {'form':SubscriptionForm()})
 
+
+def _send_mail(subject, from_, to, template_name, context):
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject,body, from_,[from_, to])
+
+
+def _success(request, form):
     _send_mail('confirmação de inscrição',settings.DEFAULT_FROM_EMAIL, 
                form.cleaned_data['email'], 
                'subscriptions/subscription_email.txt', 
@@ -24,10 +34,3 @@ def create(request):
     messages.success(request, 'Inscrição Realizada com Sucesso!')
     
     return HttpResponseRedirect('/inscricao/')
-    
-def new(request):
-    return render(request,  'subscriptions/subscription_form.html', {'form':SubscriptionForm()})
-
-def _send_mail(subject, from_, to, template_name, context):
-    body = render_to_string(template_name, context)
-    mail.send_mail(subject,body, from_,[from_, to])
